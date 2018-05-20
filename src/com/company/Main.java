16a2d47fa511ff3d15a7c867209a0df15951d2ab
David Vlaminck <david.vlaminck.y3391@student.hogent.be>
2018-05-20 11:53:25 +0200

    3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,24 @@
 package com.company;
 
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
-        for (int i = 11;i<Math.pow(10,10);i++)
+        int sum = 0;
+        BigInteger i ;
+        for (i= BigInteger.valueOf(11); i.compareTo(new BigInteger("1000000000")) < 0;i=i.add(BigInteger.ONE))
         {
-            if (isNumberRotator(i))
+            if (isNumberRotatorImproved(i)) {
                 System.out.println(i);
+                //sum += i;
+            }
         }
+        for (int j = 9; j<=99;j++)
+            sum += 55555 + 4 * 111110;
+
+        System.out.println(sum);
     }
 
     public static boolean isNumberRotator(int n)
@@ -19,6 +28,20 @@ public class Main {
         int lg = (int)Math.floor(Math.log10(n));
         var new_number = number + lastdigit* Math.pow(10,lg);
         if (new_number % n == 0)
+            return  true;
+
+        return false;
+    }
+
+    public static boolean isNumberRotatorImproved(BigInteger n)
+    {
+        String s = String.valueOf(n);
+        s = s.substring(s.length() - 1) + s.substring(0,s.length() - 1);
+        BigInteger o = new BigInteger(s);
+
+        var a = o.mod(n);
+        var cnd = a.compareTo(BigInteger.ZERO);
+        if (cnd == 0)
             return  true;
 
         return false;
